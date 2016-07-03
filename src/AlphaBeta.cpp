fe6843948886d67314deee2741703d9da4d0d291
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-07-03 15:16:14 +0200

    Renamed the class FirstStage to AlphaBeta because alpha beta pruning should not be the only possible tree search algorithm.

---
+++ src/AlphaBeta.cpp
@@ -0,0 +1,46 @@
+#include "AlphaBeta.hpp"
+
+namespace gs {
+
+namespace /* anonymous */ {
+
+std::shared_ptr<TreeNode> alpha_beta_pure_proper(
+	                                std::shared_ptr<GameState> game_state,
+					size_t depth,
+					eval_type alpha,
+					eval_type beta,
+					bool maximizing)
+{
+	std::vector< std::shared_ptr<GameState> > next_states
+		= game_state->generate_next_states();
+	return nullptr;
+}
+
+} // anonymous namespace
+
+std::shared_ptr<TreeNode> AlphaBeta::alpha_beta_pure(
+	                                std::shared_ptr<GameState> game_state,
+					size_t depth,
+					eval_type alpha,
+					eval_type beta,
+					bool maximizing) const
+{
+	static const std::vector< std::shared_ptr<TreeNode> > empty_child_vec{};
+	
+        if (!game_state) {
+		return std::make_shared<TreeNode>(game_state, 0,
+						  maximizing, empty_child_vec);
+	}
+
+	if (depth == 0 || game_state->get_winner() != GameState::NONE) {
+		return  std::make_shared<TreeNode>(game_state,
+						   game_state->evaluate(),
+						   maximizing, empty_child_vec);
+	}
+
+	// TODO.
+	return alpha_beta_pure_proper(game_state, depth,
+				      alpha, beta, maximizing);
+}
+  
+} // namespace gs
