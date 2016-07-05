e4729551b08f98416108d9c0627d3a010399bd5e
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-07-05 11:15:20 +0200

    Adjustment

--- include/TreeSearch.hpp
+++ include/TreeSearch.hpp
@@ -1,6 +1,7 @@
 #ifndef GS_TREE_SEARCH_HPP
 #define GS_TREE_SEARCH_HPP
 
+#include <cstddef>
 #include <memory>
 #include <vector>
 
@@ -12,14 +13,17 @@ namespace gs {
 
 /**
  * This interface is used in the first stage of the game solving algorithm.
- * This stage is a game tree search with alpha-beta pruning.
+ * This stage is a game tree search.
  *
  * In the future, caching (transposition table) and multithreading support may
  * be added.
  */
 template <typename Resource>
 class TreeSearch {
-
+public:
+	virtual std::shared_ptr< TreeNode<Resource> >
+	search_tree(std::shared_ptr< GameState<Resource> > game_state,
+		    std::size_t depth) = 0;
 private:
 };
 
